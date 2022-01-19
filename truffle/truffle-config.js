1973f06ca252c1d0b4ef4c1467174606274be380
avysel <avanryssel@gmail.com>
2022-01-19 13:16:08 +0100

    truffle hdwalletprovider

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,6 +1,7 @@
-/*var HDWalletProvider = require("truffle-hdwallet-provider");
-const MNEMONIC = 'YOUR WALLET KEY';
-*/
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MNEMONIC = '';
+const INFURA_API_KEY = '';
+const FROM_ADDRESS = '';
 
 module.exports = {
 
@@ -10,13 +11,14 @@ module.exports = {
       port: 8545,
       network_id: "159999" // Match any network id
     },
-    /*ropsten: {
+    ropsten: {
       provider: function() {
-        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/YOUR_API_KEY")
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/"+INFURA_API_KEY)
       },
+      from: FROM_ADDRESS,
       network_id: 3,
       gas: 4000000   //make sure this gas allocation isn't over 4M, which is the max
-    },*/
+    },
     ganache: {
       host: "localhost",
       port: 7545,
